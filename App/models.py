deb67d39e7db5884766a88be5f08ad50463e08ef
ShahabAthar25 <shahabathar25@gmail.com>
2020-12-05 21:03:56 +0500

    Added make post

--- App/models.py
+++ App/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -11,6 +12,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
+    
+    def get_absolute_url(self):
+        return reverse('home')
 
 class FAQ(models.Model):
     question = models.CharField(max_length=60)
