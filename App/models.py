53b923b08fad4afea9eb1ced24dce14c8ae41701
Shahab Athar <shahabathar103@gmail.com>
2020-12-05 16:08:21 +0500

    created blogs post and class base views

--- App/models.py
+++ App/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    context = models.TextField()
+    date_created = models.DateField()
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
