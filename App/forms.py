18b1e6f63ccb7f220867e7ff1ad543116f69f42b
ShahabAthar25 <shahabathar25@gmail.com>
2020-12-06 22:12:46 +0500

    add automatic date system

--- App/forms.py
+++ App/forms.py
@@ -1,17 +1,15 @@
 from django import forms
 from .models import Post
-from datetime import datetime
 
 class Postform(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'author', 'context', 'date_created',)
+        fields = ('title', 'author', 'context')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter The Post Title'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
             'context': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter The Post Content'}),
-            'date_created': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Date Like This: Year-Month-Day'}),
         }
 
 class EditPostForm(forms.ModelForm):
--- App/models.py
+++ App/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime, date
 
 # Create your models here.
 
@@ -8,7 +9,7 @@ class Post(models.Model):
     title = models.CharField(max_length=120)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     context = models.TextField()
-    date_created = models.DateField()
+    date_created = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
