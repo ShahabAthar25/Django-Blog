3484cabdf157289575a9c8776de83ef701b57e5a
Shahab Athar <shahabathar103@gmail.com>
2020-12-06 10:16:39 +0500

    style forms.py and edit post-detail view

---
+++ App/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Post
+from datetime import datetime
+
+class Postform(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'author', 'context', 'date_created',)
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter The Post Title'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'context': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter The Post Content'}),
+            'date_created': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Date Like This: Year-Month-Day'}),
+        }
