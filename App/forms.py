b373a209974a520982fb81df2150e42371b10019
Shahab Athar <shahabathar103@gmail.com>
2020-12-07 20:37:42 +0500

    Added Categories

--- App/forms.py
+++ App/forms.py
@@ -4,11 +4,12 @@ from .models import Post
 class Postform(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'author', 'context')
+        fields = ('title', 'author', 'context', 'category')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter The Post Title'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
             'context': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter The Post Content'}),
         }
 
--- App/models.py
+++ App/models.py
@@ -5,11 +5,21 @@ from datetime import datetime, date
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('home')
+
 class Post(models.Model):
     title = models.CharField(max_length=120)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     context = models.TextField()
     date_created = models.DateField(auto_now=True)
+    category = models.CharField(max_length=30, default='Coding')
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
