39b2c1609db55250626d82cd7e9f3db65fae69de
ShahabAthar25 <shahabathar25@gmail.com>
2020-12-07 20:37:42 +0500

    Added Categories

--- App/admin.py
+++ App/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Post, FAQ, About
+from .models import Post, FAQ, About, Category
 
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(FAQ)
 admin.site.register(About)
+admin.site.register(Category)
