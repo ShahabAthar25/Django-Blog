b41e168b5f0792749875cea8de15d220a0c5bc8b
ShahabAthar25 <shahabathar25@gmail.com>
2020-12-08 21:29:16 +0500

    Added Custom Category Error Page

--- App/forms.py
+++ App/forms.py
@@ -11,11 +11,11 @@ for item in choices:
 class Postform(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'author', 'context', 'category')
+        fields = ('title', 'author', 'category', 'context',)
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter The Post Title'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id':'username', 'type':'hidden'}),
             'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'context': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter The Post Content'}),
         }
