83888308b5d1357c5eff64dfcbb9950a74291acb
ShahabAthar25 <shahabathar25@gmail.com>
2020-12-09 22:07:09 +0500

    style signup form

--- registration/forms.py
+++ registration/forms.py
@@ -8,3 +8,10 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+    
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+    
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
