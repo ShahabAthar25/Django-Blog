c817880b5e9479c0884713912f3386874549a883
Shahab Athar <shahabathar103@gmail.com>
2020-12-10 13:32:39 +0500

    Added Password Change Page

--- registration/urls.py
+++ registration/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from .views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('sign-up/', UserRegisterView.as_view(), name='signup'),
     path('profile/', profile_view.as_view(), name='profile'),
+    path('password/', PasswordChangeView.as_view()),
 ]
